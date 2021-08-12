1dc202d945a99042e08404b7db6c09f2dec72cf9
NamLeu <flycam.cambay18@gmail.com>
2021-08-12 23:34:21 +0700

    Bai chuyen nhiet do F

---
+++ Chuyennhietdo.py
@@ -0,0 +1,3 @@
+c = int(input("Nhập nhiệt độ C: "))
+f = (9*(int(c))/5)+32
+print("Nhiệt độ F là: " + str(f))
---
+++ Test.py
@@ -0,0 +1,7 @@
+name = input("Ten nguoi gui: ")
+adress = input("Nhap dia chi: ")
+time = input("Nhap ngày tháng năm: ")
+
+print('{} thương nhớ\n'
+      ' Em không biết....'
+      ' môi khi \n{} {}'.format(name,adress,time))
