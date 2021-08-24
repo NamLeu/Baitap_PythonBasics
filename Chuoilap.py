26d22dd40615e4bd474ee0ba44be21fed18f05f8
NamLeu <flycam.cambay18@gmail.com>
2021-08-24 16:08:43 +0700

    update 2408

--- Chuoilap.py
+++ Chuoilap.py
@@ -4,5 +4,5 @@ for i in range(1, x):
     if chuoi[0:i] == chuoi[i: i + i]:
         h = x // i
         print("Lặp lại chuỗi {} số lần là {}".format(chuoi[0:i], h))
-    elif chuoi[0:i] != chuoi[i: i + i]:
-        print("Không có chuỗi lặp")
+    else:
+        continue
